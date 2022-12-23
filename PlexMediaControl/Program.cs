7589f30f1f48bc7f620a05f4451c7a3f35df59e6
Dick Kluis <dick@concise-advice.net>
2022-12-23 18:53:03 -0500

    Counting all records in the Tables

--- PlexMediaControl/Program.cs
+++ PlexMediaControl/Program.cs
@@ -5,9 +5,9 @@ Console.WriteLine("Hello, Entity Framework World!");
 var db = new TvMazeNewDbContext();
 
 var totalRecords = 0;
-var result = db.ActionItems.ToArray();
-totalRecords = result.Length;
-Console.WriteLine($"Action Items {result.Length} records found");
+var recCount= db.ActionItems.Count();
+totalRecords += recCount;
+Console.WriteLine($"Action Items {recCount} records found");
 
 var result2 = db.Episodes.ToArray();
 totalRecords += result2.Length;
@@ -19,19 +19,35 @@ Console.WriteLine($"Followed {result3.Length} records found");
 
 var result4 = db.LastShowEvaluateds.ToArray();
 totalRecords += result4.Length;
-Console.WriteLine($"Followed {result4.Length} records found");
+Console.WriteLine($"Last Evaluated Show {result4.Length} records found");
 
 var result5 = db.MediaTypes.ToArray();
 totalRecords += result5.Length;
-Console.WriteLine($"Followed {result5.Length} records found");
+Console.WriteLine($"Media Types {result5.Length} records found");
+
+var result6 = db.PlexStatuses.ToArray();
+totalRecords += result6.Length;
+Console.WriteLine($"Plex Statuses {result6.Length} records found");
+
+var result7 = db.PlexWatchedEpisodes.ToArray();
+totalRecords += result7.Length;
+Console.WriteLine($"Plex Watched Episodes {result7.Length} records found");
 
 var result1 = db.Shows.ToArray();
 totalRecords += result1.Length;
 Console.WriteLine($"Shows {result1.Length} records found");
 
+var result8 = db.ShowRssFeeds.ToArray();
+totalRecords += result8.Length;
+Console.WriteLine($"ShowRss Feeds {result8.Length} records found");
 
+var result9 = db.ShowStatuses.ToArray();
+totalRecords += result9.Length;
+Console.WriteLine($"Show Statuses {result9.Length} records found");
 
-
+var result10 = db.TvmStatuses.ToArray();
+totalRecords += result10.Length;
+Console.WriteLine($"Tvm Statuses {result10.Length} records found");
 
 Console.WriteLine($"Db has {totalRecords} records");
 //
